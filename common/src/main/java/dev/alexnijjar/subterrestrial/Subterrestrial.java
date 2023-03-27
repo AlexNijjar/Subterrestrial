a96c53f9f30784a178165022239bea8922a904bb
Alex Nijjar <alexander.nijjar@icloud.com>
2023-03-27 01:30:36 -0500

    1.19.4

--- common/src/main/java/dev/alexnijjar/subterrestrial/Subterrestrial.java
+++ common/src/main/java/dev/alexnijjar/subterrestrial/Subterrestrial.java
@@ -1,12 +1,30 @@
 package dev.alexnijjar.subterrestrial;
 
-import dev.alexnijjar.subterrestrial.registry.ModStructures;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Decoder;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.Lifecycle;
+import net.minecraft.core.Registry;
+import net.minecraft.core.WritableRegistry;
+import net.minecraft.resources.*;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.util.Locale;
+import java.util.Map;
 
 public class Subterrestrial {
     public static final String MOD_ID = "subterrestrial";
 
     public static void init() {
-        ModStructures.init();
-        ModStructures.registerMainChest();
+    }
+
+    public static <E> void loadRegistryContents(RegistryOps.RegistryInfoLookup registryInfoLookup, ResourceManager resourceManager, ResourceKey<? extends Registry<E>> resourceKey, WritableRegistry<E> writableRegistry, Decoder<E> decoder, Map<ResourceKey<?>, Exception> map, CallbackInfo ci, String string2, FileToIdConverter fileToIdConverter, RegistryOps<JsonElement> registryOps, Map.Entry<ResourceLocation, Resource> entry, ResourceLocation resourceLocation) {
+        System.out.println("Subterrestrial: loadRegistryContents");
     }
 }
